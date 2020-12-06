f62fb9b57cb189e658487cccfe1fcf996992fed1
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2020-12-07 00:50:52 +0200

    Provides access to port's tx/rx rings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use netmap_sys::{nmport_close, nmport_d, nmport_open};
+use netmap_sys::{netmap_ring, netmap_rxring, netmap_txring, nmport_close, nmport_d, nmport_open};
 
 pub struct PortSpec {
     spec: String,
@@ -6,6 +6,8 @@ pub struct PortSpec {
 
 pub struct Port {
     port: *mut nmport_d,
+    rx_rings: Vec<Ring>,
+    tx_rings: Vec<Ring>,
 }
 
 impl PortSpec {
@@ -23,7 +25,21 @@ impl PortSpec {
 impl Port {
     fn open(spec: &str) -> Result<Self, String> {
         let port = Port::open_port(spec)?;
-        Ok(Self { port })
+        let tx_rings = Port::tx_rings_from_port(&port);
+        let rx_rings = Port::rx_rings_from_port(&port);
+        Ok(Self {
+            port,
+            tx_rings,
+            rx_rings,
+        })
+    }
+
+    pub fn tx_rings(&self) -> &Vec<Ring> {
+        &self.tx_rings
+    }
+
+    pub fn rx_rings(&self) -> &Vec<Ring> {
+        &self.rx_rings
     }
 
     fn open_port(spec: &str) -> Result<*mut nmport_d, String> {
@@ -34,6 +50,36 @@ impl Port {
             Ok(port)
         }
     }
+
+    fn tx_rings_from_port(port: &*mut nmport_d) -> Vec<Ring> {
+        unsafe { (**port).first_tx_ring..=(**port).last_tx_ring }
+            .map(|ri| Port::txring_from_index(port, ri))
+            .collect()
+    }
+
+    fn rx_rings_from_port(port: &*mut nmport_d) -> Vec<Ring> {
+        unsafe { (**port).first_rx_ring..=(**port).last_rx_ring }
+            .map(|ri| Port::rxring_from_index(port, ri))
+            .collect()
+    }
+
+    fn txring_from_index(port: &*mut nmport_d, index: u16) -> Ring {
+        let ring = unsafe { netmap_txring((**port).nifp, index) };
+        Ring {
+            index,
+            ring,
+            ring_type: RingType::Tx,
+        }
+    }
+
+    fn rxring_from_index(port: &*mut nmport_d, index: u16) -> Ring {
+        let ring = unsafe { netmap_rxring((**port).nifp, index) };
+        Ring {
+            index,
+            ring,
+            ring_type: RingType::Rx,
+        }
+    }
 }
 
 impl Drop for Port {
@@ -42,7 +88,18 @@ impl Drop for Port {
     }
 }
 
-struct Ring {}
+enum RingType {
+    Rx,
+    Tx,
+}
+
+pub struct Ring {
+    index: u16,
+    ring_type: RingType,
+    ring: *mut netmap_ring,
+}
+
+impl Ring {}
 
 #[cfg(test)]
 mod tests {
