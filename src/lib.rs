af76269b0c94b1d9393b2fed8dd933f8f54a7788
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2021-01-03 02:37:47 +0200

    Provides really basic documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! # Overview
+//!
+//! **Waring**: This is experimental project, it is unstable, probably unuseable. It's aim it so
+//! try out stuff and allow me to learn Rust.
+//!
+//! This library provides Rust API to NetMap's libnetmap library.
 use std::ops::Index;
 use std::time::Duration;
 
