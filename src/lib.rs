1573613c13fc96ef212a96bea4ec667a295c1655
Alexander Dimitrov <3321085+anchorite@users.noreply.github.com>
2021-01-08 02:30:12 +0200

    Fixes typos

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Overview
 //!
-//! **Waring**: This is experimental project, it is unstable, probably unuseable. It's aim it so
+//! **Warning**: This is experimental project, it is unstable, probably unuseable. It's aim is to
 //! try out stuff and allow me to learn Rust.
 //!
 //! This library provides Rust API to NetMap's libnetmap library.
